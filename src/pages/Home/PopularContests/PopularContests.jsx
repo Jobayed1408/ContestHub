45ef1181a657c79acda2c8323deb18c5df86961b
Jobayed1408 <mdjobayedislam4@gmail.com>
2025-12-18 10:32:44 +0600

    Popular contest card add

--- src/pages/Home/PopularContests/PopularContests.jsx
+++ src/pages/Home/PopularContests/PopularContests.jsx
@@ -46,34 +46,9 @@ const PopularContests = () => {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
                 {contests.slice(0, 5).map((contest) => (
-                    // <motion.div
-                    //     key={contest._id}
-                    //     className="bg-white rounded-xl shadow-md overflow-hidden border border-gray-100 hover:shadow-xl transition"
-                    //     whileHover={{ scale: 1.03 }}
-                    // >
-                    //     <img
-                    //         src={contest.image }
-                    //         alt={contest.name}
-                    //         className="w-full h-40 object-cover"
-                    //     />
-                    //     <div className="p-4">
-                    //         <h3 className="text-lg font-bold mb-1 text-gray-800">{contest.name}</h3>
-                    //         <p className="text-gray-500 mb-2">
-                    //             {contest.description?.slice(0, 20) || "No description"}...
-                    //         </p>
-                    //         <p className="text-sm text-gray-600 mb-3">
-                    //             Participants: <span className="font-semibold">{contest.participants || 0}</span>
-                    //         </p>
-                    //         <button
-                    //             onClick={() => handleDetails(contest._id)}
-                    //             className="bg-emerald-600 text-white py-2 px-4 rounded w-full hover:bg-emerald-700 cursor-pointer transition"
-                    //         >
-                    //             Details
-                    //         </button>
-                    //     </div>
-                    // </motion.div>
-                    <motion.div>
-                        <ContestCard key={contest._id} contest={contest} />
+                   
+                    <motion.div key={contest._id}>
+                        <ContestCard  contest={contest} />
                     </motion.div>
                 ))}
             </div>
